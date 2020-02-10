b5d89331376e13b45a56d2a9fceb5c574dfbfaba
Alysson <alysson_salgado@hotmail.com>
2020-02-10 05:52:42 -0300

    Implementação metodos em categoria

--- src/main/java/com/arcs/cibus/server/resource/CategoriaResource.java
+++ src/main/java/com/arcs/cibus/server/resource/CategoriaResource.java
@@ -1,8 +1,8 @@
 package com.arcs.cibus.server.resource;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +21,49 @@ public class CategoriaResource {
 	private CategoriaService categoriaService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		return null;
+	public ResponseEntity<Page<Categoria>> getAll(int pagina, int qtdElementos) {
+		Page<Categoria> categorias = categoriaService.getAll(pagina, qtdElementos);
+		return ResponseEntity.ok(categorias);
 	}
 	
 	@RequestMapping(value="/{categoriaId}", method = RequestMethod.GET)
-	public ResponseEntity<Categoria> find(@PathVariable Long categoriaId) throws ObjectNotFoundException {
+	public ResponseEntity<Categoria> findById(@PathVariable Long categoriaId) throws ObjectNotFoundException {
 		Categoria categoria = categoriaService.findById(categoriaId);
 		return ResponseEntity.ok(categoria);
+	}
+	
+	@RequestMapping(value="/{categoriaId}", method = RequestMethod.DELETE)
+	public ResponseEntity<String> delete(@PathVariable Long categoriaId) {
+		try {
+			categoriaService.delete(categoriaId);
+			return ResponseEntity.ok(Boolean.TRUE.toString());		
+		} 
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public ResponseEntity<Categoria> update(Categoria categoria) {
+		try {
+			categoria = categoriaService.update(categoria);
+			return ResponseEntity.ok(categoria);			
+		} 
+		catch (Exception e) {
+//			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+			return null;
+		}
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Categoria> insert(Categoria categoria) {
+		try {
+			categoria = categoriaService.insert(categoria);
+			return ResponseEntity.ok(categoria);			
+		} 
+		catch (Exception e) {
+			return null;
+		}
 	}
 }
 	
--- src/main/java/com/arcs/cibus/server/service/CategoriaService.java
+++ src/main/java/com/arcs/cibus/server/service/CategoriaService.java
@@ -3,6 +3,9 @@ package com.arcs.cibus.server.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.arcs.cibus.server.domain.Categoria;
@@ -16,9 +19,27 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	public Page<Categoria> getAll(int pagina, int qtdElementos) {
+		Pageable paginacao = PageRequest.of(pagina, qtdElementos);
+		return categoriaRepository.findAll(paginacao);
+	}
+	
 	public Categoria findById(Long categoriaId) throws ObjectNotFoundException {
 		Optional<Categoria> categoria = categoriaRepository.findById(categoriaId);		
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id " + categoriaId + " não existe."));
+	}
+	
+	public void delete(Long categoriaId) {	
+		Categoria categoria = this.findById(categoriaId);
+		categoriaRepository.delete(categoria);
+	}
+	
+	public Categoria insert(Categoria categoria){
+		return categoriaRepository.save(categoria); 
+	}
+	
+	public Categoria update(Categoria categoria) {
+		return categoriaRepository.save(categoria);
 	}
 }
