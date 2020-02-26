f4aa0ab50727dc1612213ea2e57ff91d89398e51
Alysson <alysson_salgado@hotmail.com>
2020-02-26 10:24:02 -0300

    AJustes de segurança e login

--- src/main/java/com/arcs/cibus/server/resource/LoginResource.java
+++ src/main/java/com/arcs/cibus/server/resource/LoginResource.java
@@ -1,32 +1,39 @@
 package com.arcs.cibus.server.resource;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.arcs.cibus.server.domain.User;
+import com.arcs.cibus.server.security.JWTUtil;
 import com.arcs.cibus.server.service.LoginService;
 
 @RestController
-@RequestMapping(value = "/login2")
+@RequestMapping(value = "/login")
 public class LoginResource {
 	
 	@Autowired
 	private LoginService loginService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<User> login(@RequestBody User user) throws Exception {
-		user = loginService.login(user);
-		return ResponseEntity.ok(user);		
-	}
+	@Autowired
+	private JWTUtil jwtUtil;
 	
-	@RequestMapping(value="/{userId}", method = RequestMethod.POST)
-	public ResponseEntity<User> createPass(@PathVariable Long userId, @RequestBody User user) throws Exception {		
-		user = loginService.createPass(userId, user);
-		return ResponseEntity.ok(user);		
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Boolean> isAtivo(HttpServletRequest request) throws Exception {
+
+		String token = request.getHeader("Authorization");
+		
+		if(token != null && token.startsWith("Bearer ")){
+			Boolean isTokenValid = jwtUtil.isTokenValid(token);
+			return ResponseEntity.ok(isTokenValid);
+		}
+		
+		return ResponseEntity.ok(false);
 	}
 }
