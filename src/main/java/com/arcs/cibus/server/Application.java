b882d664d0f399b579a3921961b850743175e693
Alysson Salgado <alysson.salgado@gauge.com.br>
2020-03-04 16:08:07 -0300

    Image profile

--- src/main/java/com/arcs/cibus/server/Application.java
+++ src/main/java/com/arcs/cibus/server/Application.java
@@ -83,7 +83,7 @@ public class Application implements CommandLineRunner {
 				.name("Salesman")
 				.email("alysson_salgado@hotmail.com")
 				.login("vendedor")
-				.image("https://www.emaisgoias.com.br/wp-content/uploads/2020/01/Velozes-e-Furiosos-9-Filme-ganha-novo-teaser-Vin-Diesel-620x328.jpg")
+				.image("https://abrilveja.files.wordpress.com/2016/06/shia-2014-01-11-original1.jpeg")
 				.pass(passwordEncoder.encode("vendedor"))
 				.build();
 		
