3961f9d95a4962f9fbcff3514cacd5c14f04ef7c
Alysson Carvalho <alysson.carvalho@ericsson.com>
2021-03-18 16:46:51 -0300

    Ajuste de importação

--- src/main/java/com/arcs/cibus/server/Application.java
+++ src/main/java/com/arcs/cibus/server/Application.java
@@ -1,7 +1,5 @@
 package com.arcs.cibus.server;
 
-import com.arcs.cibus.server.domain.User;
-import com.sun.org.apache.xml.internal.serializer.utils.BoolStack;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
